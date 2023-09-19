50e6eff2a6971515d86e05023911598a8726e5a3
ratatyuzik <128076489+ratatyuzik@users.noreply.github.com>
2023-09-19 22:38:37 +0200

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -5,3 +5,18 @@ WHERE name LIKE '%ськ';
 SELECT name
 FROM cities
 WHERE name LIKE '%донец%';
+
+SELECT CONCAT(name, '(',region,')')
+FROM cities
+WHERE population > 100000
+ORDER BY name ASC;
+
+SELECT name, (population/40000000)*100 AS prosentik 
+FROM cities
+ORDER BY population DESC
+LIMIT 10;
+
+SELECT CONCAT(name, ' - ', (population/40000000)*100, '%')
+FROM cities
+WHERE population >= (40000000 * (0.1/100))
+ORDER BY (population/40000000)*100 DESC;
