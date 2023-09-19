e9869b69ad9f5cd30ad8635a10042fad77998681
ratatyuzik <renateremin@gmail.com>
2023-09-19 17:46:26 +0200

    First lab

--- lab1.sql
+++ lab1.sql
@@ -1,2 +1,19 @@
-Some schanges
+SELECT name,population
+FROM cities
+LIMIT 10 OFFSET 10;
 
+SELECT name 
+FROM cities
+ORDER BY name DESC
+LIMIT 30;
+
+SELECT region, population 
+FROM cities
+ORDER BY population DESC;
+
+SELECT DISTINCT region 
+FROM cities;
+
+SELECT name, region
+FROM cities 
+ORDER BY region DESC, name DESC  
