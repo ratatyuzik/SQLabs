21b6bc70c31acafd6c631f9fec05d962d5224cff
ratatyuzik <128076489+ratatyuzik@users.noreply.github.com>
2023-11-28 08:46:43 +0100

    Update lab6.sql

--- lab6.sql
+++ lab6.sql
@@ -18,17 +18,20 @@ DROP TABLE IF EXISTS `branch`;
 CREATE TABLE `branch` (
   `station_id` int(11) NOT NULL,
   `line_id` int(11) NOT NULL,
+  `order` int(11) NOT NULL,
   `id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `station_id` (`station_id`),
   KEY `line_id` (`line_id`),
-  CONSTRAINT `branch_ibfk_1` FOREIGN KEY (`station_id`) REFERENCES `station` (`id`),
+  CONSTRAINT `branch_ibfk_1` FOREIGN KEY (`station_id`) REFERENCES `stations` (`id`),
   CONSTRAINT `branch_ibfk_2` FOREIGN KEY (`line_id`) REFERENCES `lines` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `branch` (`station_id`, `line_id`, `id`) VALUES
-(1,	1,	1),
-(1,	2,	2);
+INSERT INTO `branch` (`station_id`, `line_id`,  `order`, `id`) VALUES
+(1,	1,	1, 1),
+(1,	2,	1, 2);
+(2, 1,  2, 3);
+(3, 2,  2, 4);
 
 DROP TABLE IF EXISTS `lines`;
 CREATE TABLE `lines` (
@@ -41,21 +44,18 @@ INSERT INTO `lines` (`id`, `name`) VALUES
 (1,	'Red'),
 (2,	'Green');
 
-DROP TABLE IF EXISTS `station`; 
-CREATE TABLE `station` (
-  `line_id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `stations`; 
+CREATE TABLE `stations` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` char(255) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `line_id` (`line_id`),
-  CONSTRAINT `station_ibfk_1` FOREIGN KEY (`line_id`) REFERENCES `lines` (`line_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `station` (`line_id`, `id`, `name`) VALUES
-(1,	1,	'My first station'),
-(1,	2,	'My second station'),
-(2,	3,	'My first green station'),
-(2,	4,	'My second green station');
+INSERT INTO `stations` (`line_id`, `id`, `name`, `order`) VALUES
+(1,	'My first station'),
+(2,	'My second station'),
+(3,	'My third station'),
+(4,	'My forth station');
 
 DROP TABLE IF EXISTS `transit_station`;
 CREATE TABLE `transit_station` (
@@ -65,7 +65,7 @@ CREATE TABLE `transit_station` (
   `to_line_id` int(11) NOT NULL,
   PRIMARY KEY (`transit_station_id`),
   KEY `station_id` (`station_id`),
-  CONSTRAINT `transit_station_ibfk_1` FOREIGN KEY (`station_id`) REFERENCES `station` (`id`)
+  CONSTRAINT `transit_station_ibfk_1` FOREIGN KEY (`station_id`) REFERENCES `stations` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO `transit_station` (`id`, `station_id`, `from_line_id`, `to_line_id`) VALUES
