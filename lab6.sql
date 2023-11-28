c760d108083f2ba404afb1f24a2bcc3f5758f3f4
ratatyuzik <128076489+ratatyuzik@users.noreply.github.com>
2023-11-28 07:50:16 +0100

    Update lab6.sql

--- lab6.sql
+++ lab6.sql
@@ -14,59 +14,59 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-DROP TABLE IF EXISTS `branch`; // Створив бренчі бо на 1 станції можуть бути декілька ліній
+DROP TABLE IF EXISTS `branch`;
 CREATE TABLE `branch` (
   `station_id` int(11) NOT NULL,
   `line_id` int(11) NOT NULL,
-  `branch_id` int(11) NOT NULL,
-  PRIMARY KEY (`branch_id`),
+  `id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
   KEY `station_id` (`station_id`),
   KEY `line_id` (`line_id`),
-  CONSTRAINT `branch_ibfk_1` FOREIGN KEY (`station_id`) REFERENCES `Station` (`station_id`),
-  CONSTRAINT `branch_ibfk_2` FOREIGN KEY (`line_id`) REFERENCES `lines` (`line_id`)
+  CONSTRAINT `branch_ibfk_1` FOREIGN KEY (`station_id`) REFERENCES `station` (`id`),
+  CONSTRAINT `branch_ibfk_2` FOREIGN KEY (`line_id`) REFERENCES `lines` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `branch` (`station_id`, `line_id`, `branch_id`) VALUES
+INSERT INTO `branch` (`station_id`, `line_id`, `id`) VALUES
 (1,	1,	1),
 (1,	2,	2);
 
-DROP TABLE IF EXISTS `lines`; // Лінії метрополітену
+DROP TABLE IF EXISTS `lines`;
 CREATE TABLE `lines` (
-  `line_id` int(11) NOT NULL AUTO_INCREMENT,
-  `line_name` varchar(255) NOT NULL,
-  PRIMARY KEY (`line_id`)
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `lines` (`line_id`, `line_name`) VALUES
+INSERT INTO `lines` (`id`, `name`) VALUES
 (1,	'Red'),
 (2,	'Green');
 
-DROP TABLE IF EXISTS `Station`; // Створюю станції та прив'язую до лінії
-CREATE TABLE `Station` (
+DROP TABLE IF EXISTS `station`; 
+CREATE TABLE `station` (
   `line_id` int(11) NOT NULL,
-  `station_id` int(11) NOT NULL AUTO_INCREMENT,
-  `station_name` char(255) NOT NULL,
-  PRIMARY KEY (`station_id`),
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` char(255) NOT NULL,
+  PRIMARY KEY (`id`),
   KEY `line_id` (`line_id`),
-  CONSTRAINT `Station_ibfk_1` FOREIGN KEY (`line_id`) REFERENCES `lines` (`line_id`)
+  CONSTRAINT `station_ibfk_1` FOREIGN KEY (`line_id`) REFERENCES `lines` (`line_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `Station` (`line_id`, `station_id`, `station_name`) VALUES
+INSERT INTO `station` (`line_id`, `id`, `name`) VALUES
 (1,	1,	'My first station'),
 (1,	2,	'My second station'),
 (2,	3,	'My first green station'),
 (2,	4,	'My second green station');
 
-DROP TABLE IF EXISTS `transit_station`; // id переходу між гілками на станції
+DROP TABLE IF EXISTS `transit_station`;
 CREATE TABLE `transit_station` (
-  `transit_station_id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `station_id` int(11) NOT NULL,
   `from_line_id` int(11) NOT NULL,
   `to_line_id` int(11) NOT NULL,
   PRIMARY KEY (`transit_station_id`),
   KEY `station_id` (`station_id`),
-  CONSTRAINT `transit_station_ibfk_1` FOREIGN KEY (`station_id`) REFERENCES `Station` (`station_id`)
+  CONSTRAINT `transit_station_ibfk_1` FOREIGN KEY (`station_id`) REFERENCES `station` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `transit_station` (`transit_station_id`, `station_id`, `from_line_id`, `to_line_id`) VALUES
+INSERT INTO `transit_station` (`id`, `station_id`, `from_line_id`, `to_line_id`) VALUES
 (1,	1,	1,	2);
